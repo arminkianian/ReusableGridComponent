205fdaf8c22f0db9d4f998461cbad52fe7dda9e5
ArminKianian <arminkia.dev@gmail.com>
2023-05-05 00:32:16 +0430

    Add project files.

---
+++ ReusableGridComponent/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace ReusableGridComponent.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Handle { get; set; }
+    }
+}
